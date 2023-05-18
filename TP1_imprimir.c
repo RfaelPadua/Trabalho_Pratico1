c40b0cb3434e793d13731170906de98ccb844301
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2023-05-18 20:55:31 -0300

    Add files via upload

---
+++ TP1_imprimir.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i;
+    int fita[17] = {70, -67, -10, 34, 23, 65, 87, 9, 0, -10, 4, 10, 20, 30, 9, -5, -10};
+
+    printf("Rod ");
+    for(i = 0; i < 17; i++)
+        printf("|%3d| ", i+1);
+
+    printf("\n");
+
+    printf("Pon ");
+    for(i = 0; i < 17; i++)
+        printf("|%3d| ", fita[i]);
+    
+    printf("\n");
+    system("pause");
+
+    return 0;
+}
