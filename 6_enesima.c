f306144d4b856bbe94ea8591f655991031fde804
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2023-05-26 11:53:21 -0300

    Update 6_enesima.c
    Comentarios

--- 6_enesima.c
+++ 6_enesima.c
@@ -7,21 +7,21 @@ int main() {
     int pon[17] = {70, -67, -10, 34, 23, 65, 87, 9, 0, -10, 4, 10, 20, 30, 9, -5, -10};
     int vetoraux[17] = {70, -67, -10, 34, 23, 65, 87, 9, 0, -10, 4, 10, 20, 30, 9, -5, -10};
 
-    printf("Qual posicao? ");
+    printf("Qual posicao? ");                   //Pergunta para o usuario qual melhor pontuacao ele quer.
     scanf(" %d", &menu3);
 
     do{
-        cont = 0;
+        cont = 0;                                //Variavel de controle, para saber quantos pares estao ordem certa.
         for(i = 0; i < n_rodadas - 1; i++){
-            if(vetoraux[i] < vetoraux[i + 1]){
+            if(vetoraux[i] < vetoraux[i + 1]){   //Percorre o vetor em pares, colocando em ordem se necessario
                 aux = vetoraux[i];
                 vetoraux[i] = vetoraux[i + 1];
                 vetoraux[i + 1] = aux;
             }else{
-                cont++;
+                cont++;                          //Se o par estiver na ordem certa, incrementa a variavel de controle
             }
         }
-    }while(cont < n_rodadas - 1);
+    }while(cont < n_rodadas - 1);                //Percorre o vetor ate a variavel de controle for igual a quantidades de pares.
 
     printf("A %d melhor rodada foi com o valor %d.\n", menu3, vetoraux[menu3 - 1]);
 
