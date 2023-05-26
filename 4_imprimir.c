cd29bc1d9079a4a516e66d19ced571e50b9a3f29
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2023-05-26 11:48:02 -0300

    Update 4_imprimir.c

--- 4_imprimir.c
+++ 4_imprimir.c
@@ -5,13 +5,13 @@ int main() {
     int i, n_rodadas = 17;
     int pon[17] = {70, -67, -10, 34, 23, 65, 87, 9, 0, -10, 4, 10, 20, 30, 9, -5, -10};
 
-    printf("\nRod ");
+    printf("\nRod ");// cabecalho da fita, Rod = rodada.
     for(i = 0; i < n_rodadas; i++){
-        printf("|%4d ", i+1);
-    }
+        printf("|%4d ", i+1); // %4d para reservar 4 espaços para a impressao dos numeros,
+    }                         //ja que o maior quantidade de digitos possivel na fita eh em -100
     printf("|\n");
 
-    printf("Pon ");
+    printf("Pon ");// cabecalho da fita, Pon = Pontuacao.
     for(i = 0; i < n_rodadas; i++){
         printf("|%4d ", pon[i]);
     }
