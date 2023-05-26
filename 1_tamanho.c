77e16e794cf570143f446a56a85fb9eab04318f4
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2023-05-26 11:29:52 -0300

    Update 1_tamanho.c
    Comentarios

--- 1_tamanho.c
+++ 1_tamanho.c
@@ -4,12 +4,12 @@
 int main() {
     int n_rodadas;
 
-    printf("Quantas rodadas? ");
+    printf("Quantas rodadas? ");//Pedir para o usuario definar a quantidade de rodadas;
     scanf(" %d", &n_rodadas);
 
     system("cls");
 
-    int pon[n_rodadas];
+    int pon[n_rodadas];//Criar o vetor referente as pontuacoes das rodadas.
 
     return 0;
 }
