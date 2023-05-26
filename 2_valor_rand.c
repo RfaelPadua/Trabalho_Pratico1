0d5c527fb578d156adb19fc239e80981d280eee7
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2023-05-26 11:43:22 -0300

    Update 2_valor_rand.c

--- 2_valor_rand.c
+++ 2_valor_rand.c
@@ -6,11 +6,12 @@ int main() {
     int n_rodadas = 17, i;
     int pon[17];
 
-    srand(time(NULL));
+    srand(time(NULL)); // definir a semente "time" para gerar os numeros aleatorios.
     for(i = 0; i < n_rodadas; i++){
-        pon[i] = (rand() % 201) + (-100);
-    }
-
+        pon[i] = (rand() % 201) + (-100); // Gerar numero aleatorio no range de -100 a 100, usando a funcao rand
+    }                                     // (rand() % 201) + (-100)
+                                          //Gera 201 numeros possiveis de 0 a 200 / -100 para ajustar o intervalo para -100 a 100.
+    //Layout de Impressao
     printf("\nRod ");
     for(i = 0; i < n_rodadas; i++){
         printf("|%4d ", i+1);
