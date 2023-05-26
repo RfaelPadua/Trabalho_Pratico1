53a915e1fa2b6a1e73abaa34b645d242f6730380
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2023-05-26 11:44:37 -0300

    Update 3_valor_manual.c

--- 3_valor_manual.c
+++ 3_valor_manual.c
@@ -5,11 +5,12 @@ int main() {
     int i, n_rodadas = 17;
     int pon[17];
 
-    for(i = 0; i < n_rodadas; i++){
+    for(i = 0; i < n_rodadas; i++){ // Laco para pedir para o usuario o valor de cada rodada.
         printf("Insira o valor da %d rodada: ", i + 1);
         scanf(" %d", &pon[i]);
     }
     
+    //Layout de impressao
     printf("\nRod ");
     for(i = 0; i < n_rodadas; i++){
         printf("|%4d ", i+1);
