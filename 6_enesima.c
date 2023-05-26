3a92f01c29ea3c6de7b8dee036b97659090de58b
Rafael de Padua Oliveira <rafael.padua@proton.me>
2023-05-26 16:28:53 -0300

    commit

--- 6_enesima.c
+++ 6_enesima.c
@@ -5,7 +5,7 @@ int main() {
     int i, j, aux, cont = 0, menu3;
     int n_rodadas = 17;
     int pon[17] = {70, -67, -10, 34, 23, 65, 87, 9, 0, -10, 4, 10, 20, 30, 9, -5, -10};
-    int vetoraux[17] = {70, -67, -10, 34, 23, 65, 87, 9, 0, -10, 4, 10, 20, 30, 9, -5, -10};
+    int pon_aux[17] = {70, -67, -10, 34, 23, 65, 87, 9, 0, -10, 4, 10, 20, 30, 9, -5, -10};
 
     printf("Qual posicao? ");                   //Pergunta para o usuario qual melhor pontuacao ele quer.
     scanf(" %d", &menu3);
@@ -13,17 +13,17 @@ int main() {
     do{
         cont = 0;                                //Variavel de controle, para saber quantos pares estao ordem certa.
         for(i = 0; i < n_rodadas - 1; i++){
-            if(vetoraux[i] < vetoraux[i + 1]){   //Percorre o vetor em pares, colocando em ordem se necessario
-                aux = vetoraux[i];
-                vetoraux[i] = vetoraux[i + 1];
-                vetoraux[i + 1] = aux;
+            if(pon_aux[i] < pon_aux[i + 1]){   //Percorre o vetor em pares, colocando em ordem se necessario
+                aux = pon_aux[i];
+                pon_aux[i] = pon_aux[i + 1];
+                pon_aux[i + 1] = aux;
             }else{
                 cont++;                          //Se o par estiver na ordem certa, incrementa a variavel de controle
             }
         }
     }while(cont < n_rodadas - 1);                //Percorre o vetor ate a variavel de controle for igual a quantidades de pares.
 
-    printf("A %d melhor rodada foi com o valor %d.\n", menu3, vetoraux[menu3 - 1]);
+    printf("A %d melhor rodada foi com o valor %d.\n", menu3, pon_aux[menu3 - 1]);
 
     return 0;
 }
--- main.c
+++ main.c
@@ -18,13 +18,12 @@ int main(){
     system("pause");
     system("cls");
 
-    //Definir quantas rodadas.
-    printf("Quantas rodadas? ");
+    printf("Quantas rodadas? ");//Pedir para o usuario definar a quantidade de rodadas;
     scanf(" %d", &n_rodadas);
 
     system("cls");
 
-    int pon[n_rodadas];
+    int pon[n_rodadas];//Criar o vetor referente as pontuacoes das rodadas.
     int pon_aux[n_rodadas];
 
 
@@ -38,13 +37,15 @@ int main(){
         system("cls");
 
         
-        if(menu1 == 1){ //Usar a função rand, usando "time" como semente.
-            srand(time(NULL));
+        if(menu1 == 1){ 
+            srand(time(NULL)); // definir a semente "time" para gerar os numeros aleatorios.
             for(i = 0; i < n_rodadas; i++){
-                pon[i] = (rand() % 201) + (-100);
-            }
+                pon[i] = (rand() % 201) + (-100); // Gerar numero aleatorio no range de -100 a 100, usando a funcao rand
+            }                                     // (rand() % 201) + (-100)
+                                                  //Gera 201 numeros possiveis de 0 a 200 / -100 para ajustar o intervalo para -100 a 100.
+
         }else if(menu1 == 2){
-            for(i = 0; i < n_rodadas; i++){ //Perguntar para o usuario os valores de cada rodada.
+            for(i = 0; i < n_rodadas; i++){ //Laco para pedir para o usuario o valor de cada rodada.
                 printf("Insira o valor da %d rodada: ", i + 1);
                 scanf(" %d", &pon[i]);
             }
@@ -58,13 +59,13 @@ int main(){
         }
 
         //Layout para demonstrar a fita
-        printf("\nRod ");
+        printf("\nRod ");// cabecalho da fita, Rod = rodada.
         for(i = 0; i < n_rodadas; i++){
-            printf("|%4d ", i+1);
-        }
+            printf("|%4d ", i+1); // %4d para reservar 4 espaços para a impressao dos numeros,
+        }                         //ja que o maior quantidade de digitos possivel na fita eh em -100
         printf("|\n");
 
-        printf("Pon ");
+        printf("Pon ");// cabecalho da fita, Pon = Pontuacao.
         for(i = 0; i < n_rodadas; i++){
             printf("|%4d ", pon[i]);
         }
@@ -89,14 +90,13 @@ int main(){
             case 1:
                 system("cls");
 
-                //Layout para demonstrar a fita
-                printf("\nRod ");
+                printf("\nRod ");// cabecalho da fita, Rod = rodada.
                 for(i = 0; i < n_rodadas; i++){
-                    printf("|%4d ", i+1);
-                }
+                    printf("|%4d ", i+1); // %4d para reservar 4 espaços para a impressao dos numeros,
+                }                         //ja que o maior quantidade de digitos possivel na fita eh em -100
                 printf("|\n");
 
-                printf("Pon ");
+                printf("Pon ");// cabecalho da fita, Pon = Pontuacao.
                 for(i = 0; i < n_rodadas; i++){
                     printf("|%4d ", pon[i]);
                 }
@@ -113,23 +113,21 @@ int main(){
                 system("cls");
                 int cont = 0, aux, menu3;
 
-                do{
-                printf("Qual posicao? ");
+                printf("Qual posicao? ");                   //Pergunta para o usuario qual melhor pontuacao ele quer.
                 scanf(" %d", &menu3);
-                }while(menu3 < 0 || menu3 > n_rodadas);
 
                 do{
-                    cont = 0;
-                    for(i = 0; i < (n_rodadas - 1); i++){
-                        if(pon_aux[i] < pon_aux[i + 1]){
+                    cont = 0;                                //Variavel de controle, para saber quantos pares estao ordem certa.
+                    for(i = 0; i < n_rodadas - 1; i++){
+                        if(pon_aux[i] < pon_aux[i + 1]){    //Percorre o vetor em pares, colocando em ordem se necessario
                             aux = pon_aux[i];
                             pon_aux[i] = pon_aux[i + 1];
                             pon_aux[i + 1] = aux;
                         }else{
-                            cont++;
+                            cont++;                          //Se o par estiver na ordem certa, incrementa a variavel de controle
                         }
                     }
-                }while(cont < n_rodadas - 1);
+                }while(cont < n_rodadas - 1);                //Percorre o vetor ate a variavel de controle for igual a quantidades de pares.
 
                 printf("A %d melhor rodada foi com o valor %d.\n", menu3, pon_aux[menu3 - 1]);
                 system("pause");
